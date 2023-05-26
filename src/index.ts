96980b92d7e57acdcdad44625d10ac1a37326b0d
tebanhdez <jehehe1@gmail.com>
2023-05-26 14:44:44 +0200

    Persisted endpoint payload. + Added payload validation.

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,24 @@
 import express from 'express';
 import {AppDataSource} from "./ahtdb";
 import {Telemetry} from "./entity/telemetry";
+import bodyParser from "body-parser";
+import Ajv from "ajv";
 
 const app = express();
+const ajv = new Ajv();
 
 app.get('/', (req, res) => {
     res.send('Hello, world!');
 });
+
+const telemetrySchema = {
+    type: "object",
+    properties: {
+        id:      { type: "number" },
+        reading: { type: "number" },
+    },
+    required: ["reading"],
+}
 
 app.get('/telemetry', async (req, res) => {
     if(!AppDataSource.isInitialized) {
@@ -17,15 +29,21 @@ app.get('/telemetry', async (req, res) => {
     res.send(telemetry);
 });
 
-app.post('/telemetry', async (req, res) => {
+app.post("/telemetry", bodyParser.json(),async (req, res) => {
     if(!AppDataSource.isInitialized) {
         await AppDataSource.initialize();
     }
     const telemetryRepo = AppDataSource.getRepository(Telemetry);
-    let telemetry = {reading: 1000};
-    console.log(req);
-    telemetry = await telemetryRepo.save(telemetry);
-    res.send(telemetry);
+    let telemetry = new Telemetry();
+    const isDataValid = ajv.validate(telemetrySchema, req.body);
+
+    if (isDataValid) {
+        telemetry.reading = req.body.reading;
+        telemetry = await telemetryRepo.save(telemetry);
+        res.send(telemetry);
+    } else {
+        res.send(ajv.errors!.at(0)!.message);
+    }
 });
 
 app.listen(8080, () => {
